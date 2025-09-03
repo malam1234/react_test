ccb93da547fbceffe6f11157f0501cd27523ff39
MOHAMMAD Alam <93936589+malam1234@users.noreply.github.com>
2025-09-03 17:34:38 +0100

    moved styles

--- src/components/ModalBox/CreateBookModal.tsx
+++ src/components/ModalBox/CreateBookModal.tsx
@@ -4,34 +4,7 @@ import Modal from "@mui/material/Modal";
 import TextField from "@mui/material/TextField";
 import { useState } from "react";
 import { gql, useMutation } from "urql";
-
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  pt: 2,
-  px: 4,
-  pb: 3,
-};
-
-const cornerButtonStyle = {
-  position: 'absolute',
-  top: 8,
-  right: 8,
-  minWidth: 32,
-  width: 32,
-  height: 32,
-  borderRadius: '50%',
-  fontSize: 20,
-  lineHeight: 1,
-  padding: 0,
-  zIndex: 1,
-};
+import { style, cornerButtonStyle } from './CreateBookStyles';
 
 const CreateBookModal = ({ open, handleClose }: { open: boolean, handleClose: () => void }) => {
   const CREATE_BOOKS = gql`
---
+++ src/components/ModalBox/CreateBookStyles.ts
@@ -0,0 +1,27 @@
+export const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+};
+
+export const cornerButtonStyle = {
+  position: 'absolute',
+  top: 8,
+  right: 8,
+  minWidth: 32,
+  width: 32,
+  height: 32,
+  borderRadius: '50%',
+  fontSize: 20,
+  lineHeight: 1,
+  padding: 0,
+  zIndex: 1,
+};
